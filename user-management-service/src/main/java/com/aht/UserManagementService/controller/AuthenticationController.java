d5a05a0d3f717453ad25eede33cebcdd7a275241
hungkhmt <hung11812@gmail.com>
2024-05-26 21:58:11 +0700

    update

--- user-management-service/src/main/java/com/aht/UserManagementService/controller/AuthenticationController.java
+++ user-management-service/src/main/java/com/aht/UserManagementService/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.aht.UserManagementService.controller;
 
+import com.aht.UserManagementService.dto.ApiResponse;
 import com.aht.UserManagementService.dto.request.AuthenticationRequest;
 import com.aht.UserManagementService.dto.request.IntrospectRequest;
 import com.aht.UserManagementService.dto.response.AuthenticationResponse;
@@ -47,9 +48,9 @@ public class AuthenticationController {
     }
 
     @PostMapping("/logout")
-    ResponseEntity<String> logout(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+    ApiResponse<Void> logout(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
         authenticationService.logout(request);
-        return ResponseEntity.status(HttpStatus.OK).body("Logout success");
+        return ApiResponse.<Void>builder().build();
     }
 
     @PostMapping("/refresh")
