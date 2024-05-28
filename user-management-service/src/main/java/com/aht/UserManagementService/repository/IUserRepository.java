de89e9b4ca5445074e6bbfd80fdbc4a10ee7588d
hungkhmt <hung11812@gmail.com>
2024-05-28 14:39:57 +0700

    Update user service api

--- user-management-service/src/main/java/com/aht/UserManagementService/repository/IUserRepository.java
+++ user-management-service/src/main/java/com/aht/UserManagementService/repository/IUserRepository.java
@@ -1,6 +1,7 @@
 package com.aht.UserManagementService.repository;
 
 import com.aht.UserManagementService.entity.User;
+import com.aht.UserManagementService.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 public interface IUserRepository extends JpaRepository<User, Integer> {
     List<User> findAllByUserId(Integer id);
     public User findByUsername(String username);
+    public List<User> findAllByStatus(UserStatus status);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
